33c91ded147742de70c146e9d71748c72df480b6
Claire Sbaffe <claire.sbaffe@efrei.net>
2025-12-09 11:02:44 +0100

    FEAT Step 1 - Step 3 - Refactor class

--- src/main/java/com/tdd/laboratory/Laboratory.java
+++ src/main/java/com/tdd/laboratory/Laboratory.java
@@ -1,30 +1,21 @@
 package com.tdd.laboratory;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Laboratory {
 
-    List<String> knownSubstances = new ArrayList<>();
+    List<String> knownSubstances;
     HashMap<String, Double> stocks = new HashMap<>();
 
     public Laboratory(){
-        knownSubstances.add("A");
-        knownSubstances.add("B");
-        knownSubstances.add("C");
-        knownSubstances.add("D");
+        knownSubstances = List.of("A", "B", "C", "D");
     }
 
     Double getQuantity(String substance) {
-        if(!knownSubstances.contains(substance)){
+        if(!knownSubstances.contains(substance))
             throw new IllegalArgumentException("This substance does not exist");
-        }
 
         return stocks.get(substance);
-    }
-
-    public static void main(String[] args){
-        Laboratory laboratory = new Laboratory();
     }
 }
